6fcee5dfd3e74314d803e5e2446a0563ae0002a4
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-04-09 16:25:40 +0700

    update feature

--- sanPham.js
+++ sanPham.js
@@ -2,11 +2,39 @@ const express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017';
+//var url = 'mongodb://localhost:27017';
+var url = 'mongodb+srv://binhdq:abc@123@cluster0-lkrga.mongodb.net/test?retryWrites=true&w=majority';
 
 router.get('/',async (req,res)=>{
     let client= await MongoClient.connect(url);
     let dbo = client.db("NoSQLBoosterSamples");
+    let results = await dbo.collection("SanPham").find({}).toArray();
+    res.render('allSanPham',{sanPham:results});
+})
+
+router.get('/edit', async(req,res)=>{
+    let id = req.query.id;
+    var ObjectID = require('mongodb').ObjectID;
+
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("NoSQLBoosterSamples");
+    let result = await dbo.collection("SanPham").findOne({"_id" : ObjectID(id)});
+    res.render('editSanPham',{sanPham:result});
+
+})
+//update SP
+router.post('/edit', async(req,res)=>{
+    let id = req.body.id;
+    let name = req.body.name;
+    let price = req.body.gia;
+    let newValues ={$set : {TenSP: name,Price:price}};
+    var ObjectID = require('mongodb').ObjectID;
+    let condition = {"_id" : ObjectID(id)};
+    
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("NoSQLBoosterSamples");
+    await dbo.collection("SanPham").updateOne(condition,newValues);
+    //
     let results = await dbo.collection("SanPham").find({}).toArray();
     res.render('allSanPham',{sanPham:results});
 })
